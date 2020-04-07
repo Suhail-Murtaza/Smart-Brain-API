80adc559f491c0dd8b494dae06a9a9a9425be28a
Suhail-Murtaza <suhailmurtaza910@gmail.com>
2020-04-08 00:42:20 +0500

    fixing db connection

--- server.js
+++ server.js
@@ -11,10 +11,8 @@ const image = require('./controllers/image');
 const db = knex({
     client: 'pg',
     connection: {
-      host : '127.0.0.1',
-      user : 'postgres',
-      password : 'donotworry1',
-      database : 'smart-brain'
+      connectionString : process.env.DATABASE_UTL,
+      ssl: true
     }
   });
 
