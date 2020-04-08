81ed6cec4440e79ba48aa81dcb3cec23a3c52b33
Suhail-Murtaza <suhailmurtaza910@gmail.com>
2020-04-09 01:40:07 +0500

    fixing it is working

--- server.js
+++ server.js
@@ -4,8 +4,8 @@ const bcrypt = require('bcrypt-nodejs');
 const cors = require('cors');
 const knex = require('knex');
 
-const signup = require('./controllers/signup');
-const login = require('./controllers/login');
+const register = require('./controllers/register');
+const signin = require('./controllers/signin');
 const profile = require('./controllers/profile');
 const image = require('./controllers/image');
 
@@ -13,7 +13,7 @@ const db = knex({
   client: 'pg',
   connection: {
     connectionString : process.env.DATABASE_URL,
-    ssl: true,
+    ssl : true,
   }
 });
 
@@ -22,13 +22,13 @@ const app = express();
 app.use(cors())
 app.use(bodyParser.json());
 
-app.get('/', (req, res)=> { res.send('It is working') })
-app.post('/login', login.handleLogin(db, bcrypt))
-app.post('/signup', (req, res) => { signup.handleSignUp(req, res, db, bcrypt) })
+app.get('/', (req, res)=> { res.send(db.users) })
+app.post('/signin', signin.handleSignin(db, bcrypt))
+app.post('/register', (req, res) => { register.handleRegister(req, res, db, bcrypt) })
 app.get('/profile/:id', (req, res) => { profile.handleProfileGet(req, res, db)})
 app.put('/image', (req, res) => { image.handleImage(req, res, db)})
 app.post('/imageurl', (req, res) => { image.handleApiCall(req, res)})
 
-app.listen(process.env.PORT || 3000, ()=> {
+app.listen(process.env.PORT || 3000 , ()=> {
   console.log(`app is running on port ${process.env.PORT}`);
 })
